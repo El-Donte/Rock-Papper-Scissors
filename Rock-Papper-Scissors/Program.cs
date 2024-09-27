714ec0804c76f8cd7eddb3da38f174d55e0f22bf
Никита Абатуров <nikitaabaturov49@gmail.com>
2024-09-27 22:29:14 +0500

    Добавил перечисление для камня ножниц и бумаги, и выбор игрока.

--- Rock-Papper-Scissors/Program.cs
+++ Rock-Papper-Scissors/Program.cs
@@ -1,7 +1,23 @@
-﻿namespace Rock_Papper_Scissors
+﻿namespace Rock_Paper_Scissors
 {
     internal class Program
     {
+        enum Choises { Rock,Paer,Scissors}
+        static int playerChoice = 0;
+        static void PlayerChoice()
+        {
+            Console.WriteLine("1.Камень\n2.Ножницы\n3.Бумага");
+            Console.Write("Введите выбопр: ");
+            try
+            {
+                playerChoice = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
