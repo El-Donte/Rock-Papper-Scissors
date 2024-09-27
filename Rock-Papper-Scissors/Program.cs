6ad8e021a7f894d769b8fe2e58525e5a6ff693f9
Никита Абатуров <nikitaabaturov49@gmail.com>
2024-09-27 22:53:07 +0500

    Добавил логику игры

--- Rock-Papper-Scissors/Program.cs
+++ Rock-Papper-Scissors/Program.cs
@@ -2,16 +2,16 @@
 {
     internal class Program
     {
-        enum Choises { Rock = 1,Paer = 2,Scissors = 3}
-        static int playerChoice = 0;
-        static int computerChoice = 0;
+        enum Choises { Rock = 1,Scissors = 2, Paper = 3 }
+        static Choises playerChoice = 0;
+        static Choises computerChoice = 0;
         static void MakePlayerChoice()
         {
             Console.WriteLine("1.Камень\n2.Ножницы\n3.Бумага");
             Console.Write("Введите выбор: ");
             try
             {
-                playerChoice = int.Parse(Console.ReadLine());
+                playerChoice =(Choises)int.Parse(Console.ReadLine());
             }
             catch (Exception e)
             {
@@ -21,14 +21,58 @@
 
         static void MakeComputerChoice()
         {
-            Console.Write("Выбор компьютера");
+            Console.WriteLine("Выбор компьютера");
             Random rand = new Random();
-            computerChoice = rand.Next(1,4);
+            computerChoice = (Choises)rand.Next(1,4);
+        }
+
+        static void Game()
+        {
+            Console.WriteLine("Камень ножницы бумага");
+            int choice = 1;
+            do
+            {
+                try
+                {
+                    MakePlayerChoice();
+                    MakeComputerChoice();
+                    Console.WriteLine($"Выбор игрока: {playerChoice}");
+                    Console.WriteLine($"Выбор компьютера: {computerChoice}");
+                    if (playerChoice == computerChoice)
+                    {
+                        Console.WriteLine("Ничья");
+                    }
+                    else if (playerChoice == Choises.Scissors && computerChoice != Choises.Rock)
+                    {
+                        Console.WriteLine("Игрок победил");
+                    }
+                    else if (playerChoice == Choises.Rock && computerChoice != Choises.Paper)
+                    {
+                        Console.WriteLine("Игрок победил");
+                    }
+                    else if (playerChoice == Choises.Paper && computerChoice != Choises.Scissors)
+                    {
+                        Console.WriteLine("Игрок победил");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Компьютер победил");
+                    }
+                    Console.WriteLine("Хотите повторить ?\n 1.Да\n 2.Нет");
+                    choice = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                }
+                catch (Exception e) 
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            } 
+            while (choice != 2);
         }
 
         static void Main(string[] args)
         {
-            MakeComputerChoice();
+            Game();
         }
     }
 }
