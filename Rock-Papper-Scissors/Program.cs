716cc8d4cbee2a7acb1e8f855a1efe41542f8dd5
Никита Абатуров <nikitaabaturov49@gmail.com>
2024-09-27 22:15:34 +0500

    Add project files.

---
+++ Rock-Papper-Scissors/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Rock_Papper_Scissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
