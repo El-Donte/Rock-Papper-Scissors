fba9555a4e9f11644dba6c2d92c804ef81ef3440
Никита Абатуров <nikitaabaturov49@gmail.com>
2024-09-27 22:39:10 +0500

    создал выбор компьютера

--- Rock-Papper-Scissors/Program.cs
+++ Rock-Papper-Scissors/Program.cs
@@ -2,12 +2,13 @@
 {
     internal class Program
     {
-        enum Choises { Rock,Paer,Scissors}
+        enum Choises { Rock = 1,Paer = 2,Scissors = 3}
         static int playerChoice = 0;
-        static void PlayerChoice()
+        static int computerChoice = 0;
+        static void MakePlayerChoice()
         {
             Console.WriteLine("1.Камень\n2.Ножницы\n3.Бумага");
-            Console.Write("Введите выбопр: ");
+            Console.Write("Введите выбор: ");
             try
             {
                 playerChoice = int.Parse(Console.ReadLine());
@@ -18,9 +19,16 @@
             }
         }
 
+        static void MakeComputerChoice()
+        {
+            Console.Write("Выбор компьютера");
+            Random rand = new Random();
+            computerChoice = rand.Next(1,4);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MakeComputerChoice();
         }
     }
 }
